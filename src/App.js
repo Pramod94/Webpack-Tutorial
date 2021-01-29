2e3aa9950751162340895aaec4145a1923202d44
pmdm333 <pramoda.m@robosoftin.com>
2021-01-29 18:35:56 +0530

    Added Info page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Info from "./Info";
 
-class App extends Component{
-    render(){
-        return(
-            <div>From App.js</div>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div>From App.js</div>
+        <Info />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/Info.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Info = () => {
+  return <div>From Info component</div>;
+};
+
+export default Info;
